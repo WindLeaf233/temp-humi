21bce9840448c50e9ae44fd213c9ee1bb58f1e52
WindLeaf233 <mc.windleaf@foxmail.com>
2022-08-21 18:33:56 +0800

    :fire: remove default code

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VeLine from 'v-charts/lib/line.common'
 
-createApp(App).mount('#app')
+createApp(App)
+  .component('VeLine', VeLine)
+  .mount('#app')
