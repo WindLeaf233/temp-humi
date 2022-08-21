5ea9a1f54f9eb62095f79b23a5eb226516c6dd20
WindLeaf233 <mc.windleaf@foxmail.com>
2022-08-22 00:05:24 +0800

    :bug: use setup script

--- src/components/TempHumi.vue
+++ src/components/TempHumi.vue
@@ -2,7 +2,7 @@
   <v-chart class="chart" :option="option"></v-chart>
 </template>
 
-<script>
+<script setup>
 import { use } from 'echarts/core'
 import { CanvasRenderer } from 'echarts/renderers'
 import { LineChart } from 'echarts/charts'
@@ -10,8 +10,8 @@ import {
   TitleComponent, LegendComponent, GridComponent, TooltipComponent,
   DataZoomComponent, ToolboxComponent, VisualMapComponent
 } from 'echarts/components'
-import VChart, { THEME_KEY } from 'vue-echarts'
-import { ref, defineComponent, defineProps } from 'vue'
+import VChart from 'vue-echarts'
+import { ref, defineProps } from 'vue'
 import { createDeviceDetector } from 'next-vue-device-detector'
 
 const props = defineProps(['loadingInstance'])
@@ -30,162 +30,149 @@ use([
 
 const device = createDeviceDetector()
 
-export default defineComponent({
-  name: 'TempHumi',
-  components: {
-    VChart
-  },
-  provide: {
-    [THEME_KEY]: 'light'
-  },
-  setup: () => {
-    const originalData = [
-      { addtime: '08/21 05:23', temp: 40, humi: 50 },
-      { addtime: '08/21 05:24', temp: 30, humi: 60 },
-      { addtime: '08/21 05:25', temp: 20, humi: 30 },
-      { addtime: '08/21 05:26', temp: -25, humi: 10 }
-    ]
+const originalData = [
+  { addtime: '08/21 05:23', temp: 40, humi: 50 },
+  { addtime: '08/21 05:24', temp: 30, humi: 60 },
+  { addtime: '08/21 05:25', temp: 20, humi: 30 },
+  { addtime: '08/21 05:26', temp: -25, humi: 10 }
+]
 
-    function getTimeList() {
-      let temp = []
-      for (let column of originalData) {
-        temp.push(column.addtime)
-      }
-      return temp
-    }
-
-    function getDataList(type) {
-      let temp = []
-      for (let column of originalData) {
-        temp.push(column[type])
-      }
-      return temp
-    }
-
-    const option = ref({
-      title: {
-        text: '温湿度变化',
-        left: 'center'
-      },
-      legend: {
-        data: ['温度', '湿度'],
-        left: 5
-      },
-      visualMap: [
-        {
-          show: false,
-          type: 'continuous',
-          seriesIndex: 0,
-          min: -30,
-          max: 50,
-          color: ['#e64343', '#ffaa6d', '#32c6ce']
-        }
-      ],
-      grid: {
-        left: device.mobile ? '10%' : '3%',
-        right: device.mobile ? '10%' : '3%'
-      },
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'cross',
-          animation: true,
-          label: {
-            backgroundColor: '#505765'
-          }
-        }
-      },
-      toolbox: {
-        feature: {
-          dataZoom: {
-            yAxisIndex: 'none'
-          },
-          restore: {}
-        }
-      },
-      dataZoom: [
-        {
-          show: true,
-          realtime: true,
-          start: 0,
-          end: 100
-        },
-        {
-          type: 'inside',
-          realtime: true,
-          start: 0,
-          end: 100
-        }
-      ],
-      xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        data: getTimeList()
-      },
-      yAxis: [
-        {
-          name: '温度',
-          type: 'value',
-          axisLabel: {
-            formatter: '{value}℃'
-          }
-        },
-        {
-          name: '湿度',
-          type: 'value',
-          axisLabel: {
-            formatter: '{value}%'
-          }
-        }
-      ],
-      series: [
-        {
-          name: '温度',
-          type: 'line',
-          smooth: true,
-          data: getDataList('temp'),
-          markPoint: {
-            data: [
-              { type: 'max', name: '最高温度' },
-              { type: 'min', name: '最低温度' }
-            ]
-          },
-          markLine: {
-            data: [{ type: 'average', name: '平均温度' }]
-          },
-          showSymbol: false,
-          lineStyle: {
-            width: 2
-          }
-        },
-        {
-          name: '湿度',
-          type: 'line',
-          smooth: true,
-          yAxisIndex: 1,
-          data: getDataList('humi'),
-          markPoint: {
-            data: [
-              { type: 'max', name: '最高湿度' },
-              { type: 'min', name: '最低湿度' }
-            ]
-          },
-          markLine: {
-            data: [{ type: 'average', name: '平均湿度' }]
-          },
-          showSymbol: false,
-          lineStyle: {
-            width: 2
-          }
-        }
-      ]
-    })
-
-    props.loadingInstance.close()
-
-    return { option }
+function getTimeList() {
+  let temp = []
+  for (let column of originalData) {
+    temp.push(column.addtime)
   }
+  return temp
+}
+
+function getDataList(type) {
+  let temp = []
+  for (let column of originalData) {
+    temp.push(column[type])
+  }
+  return temp
+}
+
+const option = ref({
+  title: {
+    text: '温湿度变化',
+    left: 'center'
+  },
+  legend: {
+    data: ['温度', '湿度'],
+    left: 5
+  },
+  visualMap: [
+    {
+      show: false,
+      type: 'continuous',
+      seriesIndex: 0,
+      min: -30,
+      max: 50,
+      color: ['#e64343', '#ffaa6d', '#32c6ce']
+    }
+  ],
+  grid: {
+    left: device.mobile ? '10%' : '3%',
+    right: device.mobile ? '10%' : '3%'
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+      animation: true,
+      label: {
+        backgroundColor: '#505765'
+      }
+    }
+  },
+  toolbox: {
+    feature: {
+      dataZoom: {
+        yAxisIndex: 'none'
+      },
+      restore: {}
+    }
+  },
+  dataZoom: [
+    {
+      show: true,
+      realtime: true,
+      start: 0,
+      end: 100
+    },
+    {
+      type: 'inside',
+      realtime: true,
+      start: 0,
+      end: 100
+    }
+  ],
+  xAxis: {
+    type: 'category',
+    boundaryGap: false,
+    data: getTimeList()
+  },
+  yAxis: [
+    {
+      name: '温度',
+      type: 'value',
+      axisLabel: {
+        formatter: '{value}℃'
+      }
+    },
+    {
+      name: '湿度',
+      type: 'value',
+      axisLabel: {
+        formatter: '{value}%'
+      }
+    }
+  ],
+  series: [
+    {
+      name: '温度',
+      type: 'line',
+      smooth: true,
+      data: getDataList('temp'),
+      markPoint: {
+        data: [
+          { type: 'max', name: '最高温度' },
+          { type: 'min', name: '最低温度' }
+        ]
+      },
+      markLine: {
+        data: [{ type: 'average', name: '平均温度' }]
+      },
+      showSymbol: false,
+      lineStyle: {
+        width: 2
+      }
+    },
+    {
+      name: '湿度',
+      type: 'line',
+      smooth: true,
+      yAxisIndex: 1,
+      data: getDataList('humi'),
+      markPoint: {
+        data: [
+          { type: 'max', name: '最高湿度' },
+          { type: 'min', name: '最低湿度' }
+        ]
+      },
+      markLine: {
+        data: [{ type: 'average', name: '平均湿度' }]
+      },
+      showSymbol: false,
+      lineStyle: {
+        width: 2
+      }
+    }
+  ]
 })
+
+props.loadingInstance.close()
 
 // import { LinesChart } from 'echarts/charts'
 //
